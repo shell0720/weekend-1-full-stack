3d40beaaca7524985d4a72712d1131e76a5c9f9f
shell0720 <shell0720@gmail.com>
2016-03-17 20:40:27 -0500

    add modules

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var index = require("./routes/index");
+var people = require("./routes/employee");
 var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
@@ -43,81 +45,9 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use("/people", people);
+app.use("/", index);
 
-
-
-// example POST request using curl
-app.post('/people', function (req, res) {
-  console.log('body: ', req.body);
-  var first_name = req.body.firstname;
-  var last_name = req.body.lastname;
-  var employee_id = req.body.number;
-  var title = req.body.title;
-  var salary = req.body.salary;
-
-  // connect to DB
-  pg.connect(connectionString, function (err, client, done) {
-    if (err) {
-      done();
-      console.log('Error connecting to DB: ', err);
-      res.status(500).send(err);
-    } else {
-      var result = [];
-
-      var query = client.query('INSERT INTO people (first_name, last_name, employee_id, title, salary) VALUES ($1, $2, $3, $4, $5) ' +
-      'RETURNING id, first_name, last_name, employee_id, title, salary', [first_name, last_name, employee_id, title, salary]);
-
-      query.on('row', function (row) {
-        result.push(row);
-      });
-
-      query.on('end', function () {
-        done();
-        res.send(result);
-      });
-
-      query.on('error', function (error) {
-        console.log('Error running query:', error);
-        done();
-        res.status(500).send(error);
-      });
-    }
-  });
-});
-
-app.get('/people', function (req, res) {
-  // connect to DB
-  pg.connect(connectionString, function(err, client, done){
-    if (err) {
-      done();
-      console.log('Error connecting to DB: ', err);
-      res.status(500).send(err);
-    } else {
-      var result = [];
-      var query = client.query('SELECT * FROM people;');
-
-      query.on('row', function(row){
-        result.push(row);
-      });
-
-      query.on('end', function() {
-        done();
-        res.send(result);
-      });
-
-      query.on('error', function(error) {
-        console.log('Error running query:', error);
-        done();
-        res.status(500).send(error);
-      });
-    }
-  })
-});
-
-app.get('/*', function(req, res){
-  var filename = req.params[0] || 'views/index.html';
-  res.sendFile(path.join(__dirname, '/public/', filename)); // ..../server/public/filename
-});
 
 app.listen(port, function() {
   console.log('Listening for requests on port', port);
---
+++ server/routes/employee.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router = express.Router();
+var pg = require('pg');
+
+var connectionString;
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+  connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = 'postgres://localhost:5432/employee_salary';
+}
+
+router.post('/', function (req, res) {
+  console.log('body: ', req.body);
+  var first_name = req.body.firstname;
+  var last_name = req.body.lastname;
+  var employee_id = req.body.number;
+  var title = req.body.title;
+  var salary = req.body.salary;
+
+  // connect to DB
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      done();
+      console.log('Error connecting to DB: ', err);
+      res.status(500).send(err);
+    } else {
+      var result = [];
+
+      var query = client.query('INSERT INTO people (first_name, last_name, employee_id, title, salary) VALUES ($1, $2, $3, $4, $5) ' +
+      'RETURNING id, first_name, last_name, employee_id, title, salary', [first_name, last_name, employee_id, title, salary]);
+
+      query.on('row', function (row) {
+        result.push(row);
+      });
+
+      query.on('end', function () {
+        done();
+        res.send(result);
+      });
+
+      query.on('error', function (error) {
+        console.log('Error running query:', error);
+        done();
+        res.status(500).send(error);
+      });
+    }
+  });
+});
+
+router.get('/', function (req, res) {
+  // connect to DB
+  pg.connect(connectionString, function(err, client, done){
+    if (err) {
+      done();
+      console.log('Error connecting to DB: ', err);
+      res.status(500).send(err);
+    } else {
+      var result = [];
+      var query = client.query('SELECT * FROM people;');
+
+      query.on('row', function(row){
+        result.push(row);
+      });
+
+      query.on('end', function() {
+        done();
+        res.send(result);
+      });
+
+      query.on('error', function(error) {
+        console.log('Error running query:', error);
+        done();
+        res.status(500).send(error);
+      });
+    }
+  })
+});
+
+module.exports = router;
